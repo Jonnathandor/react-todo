0f18f9ce973ca8f1838f46ee593bccac79f21063
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-07-18 14:05:19 -0500

    added proto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState(['Refactor this app', 'dinner time']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Proto TODO app</h1>
+      <form>
+        <input value={input}
+        onChange={event => setInput(event.target.value)}
+        />
+        <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
+
+      <ul>
+        {todos.map(todo => {
+          return <li>{todo}</li>
+        })}
+      </ul>
     </div>
   );
 }
